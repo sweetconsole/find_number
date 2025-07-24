aaa8d1db47725614b74475286a01511f716646df
sweetconsole <sweetconsole@gmail.com>
2025-07-24 23:25:18 +0300

    Добавление стратого экрана

---
+++ src/scripts/script.js
@@ -0,0 +1,10 @@
+const intro = document.getElementById("gameIntro");
+const startWindow = document.getElementById("startWindow");
+
+function start() {
+	intro.style.display = "none"
+	startWindow.style.display = "flex";
+}
+
+intro.style.display = "none"
+startWindow.style.display = "flex";
